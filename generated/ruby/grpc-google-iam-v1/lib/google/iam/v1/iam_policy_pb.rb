5f62e40ca107748ae32fdedd740a6e14791930ea
Ernest Landrito <landrito@google.com>
2016-09-29 18:32:48 -0700

    Updates to grpc iam mixin.

--- generated/ruby/grpc-google-iam-v1/lib/google/iam/v1/iam_policy_pb.rb
+++ generated/ruby/grpc-google-iam-v1/lib/google/iam/v1/iam_policy_pb.rb
@@ -3,6 +3,7 @@
 
 require 'google/protobuf'
 
+require 'google/api/annotations_pb'
 require 'google/iam/v1/policy_pb'
 Google::Protobuf::DescriptorPool.generated_pool.build do
   add_message "google.iam.v1.SetIamPolicyRequest" do
