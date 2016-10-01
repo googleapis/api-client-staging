95d1ddfa1cce2d9f16fd31bd5d4fe1302c1b5a5e
Jacob Geiger <jgeiger@google.com>
2016-09-30 17:36:34 -0700

    Add Python Trace at 0.11.1 (#28)

--- generated/python/grpc-google-devtools-cloudtrace-v1/README.rst
+++ generated/python/grpc-google-devtools-cloudtrace-v1/README.rst
@@ -1,5 +1,5 @@
-gRPC library for google-devtools-cloudtrace-v1
+gRPC library for Stackdriver Trace
 
-grpc-google-devtools-cloudtrace-v1 is the IDL-derived library for the google-devtools-cloudtrace (v1) service in the googleapis_ repository.
+grpc-google-devtools-cloudtrace-v1 is the IDL-derived library for the Stackdriver Trace (v1) service in the googleapis_ repository.
 
 .. _`googleapis`: https://github.com/googleapis/googleapis/tree/master/google/devtools/cloudtrace/v1
--- generated/python/grpc-google-devtools-cloudtrace-v1/google/devtools/cloudtrace/v1/trace_pb2.py
+++ generated/python/grpc-google-devtools-cloudtrace-v1/google/devtools/cloudtrace/v1/trace_pb2.py
@@ -612,8 +612,8 @@ class TraceServiceServicer(object):
     raise NotImplementedError('Method not implemented!')
 
   def PatchTraces(self, request, context):
-    """Sends new traces to Cloud Trace or updates existing traces. If the ID of
-    a trace that you send matches that of an existing trace, any fields
+    """Sends new traces to Stackdriver Trace or updates existing traces. If the ID
+    of a trace that you send matches that of an existing trace, any fields
     in the existing trace and its spans are overwritten by the provided values,
     and any new fields provided are merged with the existing trace data. If the
     ID does not match, a new trace is created.
@@ -662,8 +662,8 @@ class BetaTraceServiceServicer(object):
     """
     context.code(beta_interfaces.StatusCode.UNIMPLEMENTED)
   def PatchTraces(self, request, context):
-    """Sends new traces to Cloud Trace or updates existing traces. If the ID of
-    a trace that you send matches that of an existing trace, any fields
+    """Sends new traces to Stackdriver Trace or updates existing traces. If the ID
+    of a trace that you send matches that of an existing trace, any fields
     in the existing trace and its spans are overwritten by the provided values,
     and any new fields provided are merged with the existing trace data. If the
     ID does not match, a new trace is created.
@@ -689,8 +689,8 @@ class BetaTraceServiceStub(object):
     raise NotImplementedError()
   GetTrace.future = None
   def PatchTraces(self, request, timeout, metadata=None, with_call=False, protocol_options=None):
-    """Sends new traces to Cloud Trace or updates existing traces. If the ID of
-    a trace that you send matches that of an existing trace, any fields
+    """Sends new traces to Stackdriver Trace or updates existing traces. If the ID
+    of a trace that you send matches that of an existing trace, any fields
     in the existing trace and its spans are overwritten by the provided values,
     and any new fields provided are merged with the existing trace data. If the
     ID does not match, a new trace is created.
