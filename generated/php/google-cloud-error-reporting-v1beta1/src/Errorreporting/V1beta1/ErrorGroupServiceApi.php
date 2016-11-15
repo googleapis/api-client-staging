6f08ee16153509e6a03d3fe5245bea6c32ba793a
michaelbausor <michaelbausor@google.com>
2016-11-15 16:04:57 -0600

    Regenerate all APIs (#101)
    * Move to google-cloud
    * Regenerate all

--- generated/php/google-cloud-error-reporting-v1beta1/src/Errorreporting/V1beta1/ErrorGroupServiceApi.php
+++ generated/php/google-cloud-error-reporting-v1beta1/src/Errorreporting/V1beta1/ErrorGroupServiceApi.php
@@ -16,38 +16,37 @@
 /*
  * GENERATED CODE WARNING
  * This file was generated from the file
- * https://github.com/google/googleapis/blob/master/google/cloud/speech/v1beta1/cloud_speech.proto
+ * https://github.com/google/googleapis/blob/master/google/devtools/clouderrorreporting/v1beta1/error_group_service.proto
  * and updates to that file get reflected here through a refresh process.
  */
 
-namespace Google\Cloud\Speech\V1beta1;
+namespace Google\Cloud\Errorreporting\V1beta1;
 
 use Google\GAX\AgentHeaderDescriptor;
 use Google\GAX\ApiCallable;
 use Google\GAX\CallSettings;
 use Google\GAX\GrpcConstants;
 use Google\GAX\GrpcCredentialsHelper;
-use google\cloud\speech\v1beta1\AsyncRecognizeRequest;
-use google\cloud\speech\v1beta1\RecognitionAudio;
-use google\cloud\speech\v1beta1\RecognitionConfig;
-use google\cloud\speech\v1beta1\SpeechClient;
-use google\cloud\speech\v1beta1\SyncRecognizeRequest;
+use Google\GAX\PathTemplate;
+use google\devtools\clouderrorreporting\v1beta1\ErrorGroup;
+use google\devtools\clouderrorreporting\v1beta1\ErrorGroupServiceClient;
+use google\devtools\clouderrorreporting\v1beta1\GetGroupRequest;
+use google\devtools\clouderrorreporting\v1beta1\UpdateGroupRequest;
 
 /**
- * Service Description: Service that implements Google Cloud Speech API.
+ * Service Description: Service for retrieving and updating individual error groups.
  *
  * This class provides the ability to make remote calls to the backing service through method
  * calls that map to API methods. Sample code to get started:
  *
  * ```
  * try {
- *     $speechApi = new SpeechApi();
- *     $config = new RecognitionConfig();
- *     $audio = new RecognitionAudio();
- *     $response = $speechApi->syncRecognize($config, $audio);
+ *     $errorGroupServiceApi = new ErrorGroupServiceApi();
+ *     $formattedGroupName = ErrorGroupServiceApi::formatGroupName("[PROJECT]", "[GROUP]");
+ *     $response = $errorGroupServiceApi->getGroup($formattedGroupName);
  * } finally {
- *     if (isset($speechApi)) {
- *         $speechApi->close();
+ *     if (isset($errorGroupServiceApi)) {
+ *         $errorGroupServiceApi->close();
  *     }
  * }
  * ```
@@ -57,12 +56,12 @@ use google\cloud\speech\v1beta1\SyncRecognizeRequest;
  * a parse method to extract the individual identifiers contained within names that are
  * returned.
  */
-class SpeechApi
+class ErrorGroupServiceApi
 {
     /**
      * The default address of the service.
      */
-    const SERVICE_ADDRESS = 'speech.googleapis.com';
+    const SERVICE_ADDRESS = 'clouderrorreporting.googleapis.com';
 
     /**
      * The default port of the service.
@@ -78,11 +77,52 @@ class SpeechApi
     const _CODEGEN_NAME = 'GAPIC';
     const _CODEGEN_VERSION = '0.0.0';
 
+    private static $groupNameTemplate;
+
     private $grpcCredentialsHelper;
-    private $speechStub;
+    private $errorGroupServiceStub;
     private $scopes;
     private $defaultCallSettings;
     private $descriptors;
+
+    /**
+     * Formats a string containing the fully-qualified path to represent
+     * a group resource.
+     */
+    public static function formatGroupName($project, $group)
+    {
+        return self::getGroupNameTemplate()->render([
+            'project' => $project,
+            'group' => $group,
+        ]);
+    }
+
+    /**
+     * Parses the project from the given fully-qualified path which
+     * represents a group resource.
+     */
+    public static function parseProjectFromGroupName($groupName)
+    {
+        return self::getGroupNameTemplate()->match($groupName)['project'];
+    }
+
+    /**
+     * Parses the group from the given fully-qualified path which
+     * represents a group resource.
+     */
+    public static function parseGroupFromGroupName($groupName)
+    {
+        return self::getGroupNameTemplate()->match($groupName)['group'];
+    }
+
+    private static function getGroupNameTemplate()
+    {
+        if (self::$groupNameTemplate == null) {
+            self::$groupNameTemplate = new PathTemplate('projects/{project}/groups/{group}');
+        }
+
+        return self::$groupNameTemplate;
+    }
 
     private static function getPageStreamingDescriptors()
     {
@@ -100,14 +140,14 @@ class SpeechApi
      *                       Optional. Options for configuring the service API wrapper.
      *
      *     @type string $serviceAddress The domain name of the API remote host.
-     *                                  Default 'speech.googleapis.com'.
+     *                                  Default 'clouderrorreporting.googleapis.com'.
      *     @type mixed $port The port on which to connect to the remote host. Default 443.
      *     @type Grpc\ChannelCredentials $sslCreds
      *           A `ChannelCredentials` for use with an SSL-enabled channel.
      *           Default: a credentials object returned from
      *           Grpc\ChannelCredentials::createSsl()
      *     @type array $scopes A string array of scopes to use when acquiring credentials.
-     *                         Default the scopes for the Google Cloud Speech API.
+     *                         Default the scopes for the Stackdriver Error Reporting API.
      *     @type array $retryingOverride
      *           An associative array of string => RetryOptions, where the keys
      *           are method names (e.g. 'createFoo'), that overrides default retrying
@@ -153,19 +193,19 @@ class SpeechApi
 
         $defaultDescriptors = ['headerDescriptor' => $headerDescriptor];
         $this->descriptors = [
-            'syncRecognize' => $defaultDescriptors,
-            'asyncRecognize' => $defaultDescriptors,
+            'getGroup' => $defaultDescriptors,
+            'updateGroup' => $defaultDescriptors,
         ];
         $pageStreamingDescriptors = self::getPageStreamingDescriptors();
         foreach ($pageStreamingDescriptors as $method => $pageStreamingDescriptor) {
             $this->descriptors[$method]['pageStreamingDescriptor'] = $pageStreamingDescriptor;
         }
 
-        $clientConfigJsonString = file_get_contents(__DIR__.'/resources/speech_client_config.json');
+        $clientConfigJsonString = file_get_contents(__DIR__.'/resources/error_group_service_client_config.json');
         $clientConfig = json_decode($clientConfigJsonString, true);
         $this->defaultCallSettings =
                 CallSettings::load(
-                    'google.cloud.speech.v1beta1.Speech',
+                    'google.devtools.clouderrorreporting.v1beta1.ErrorGroupService',
                     $clientConfig,
                     $options['retryingOverride'],
                     GrpcConstants::getStatusCodeNames(),
@@ -181,11 +221,11 @@ class SpeechApi
         $grpcCredentialsHelperOptions = array_diff_key($options, $defaultOptions);
         $this->grpcCredentialsHelper = new GrpcCredentialsHelper($this->scopes, $grpcCredentialsHelperOptions);
 
-        $createSpeechStubFunction = function ($hostname, $opts) {
-            return new SpeechClient($hostname, $opts);
+        $createErrorGroupServiceStubFunction = function ($hostname, $opts) {
+            return new ErrorGroupServiceClient($hostname, $opts);
         };
-        $this->speechStub = $this->grpcCredentialsHelper->createStub(
-            $createSpeechStubFunction,
+        $this->errorGroupServiceStub = $this->grpcCredentialsHelper->createStub(
+            $createErrorGroupServiceStubFunction,
             $options['serviceAddress'],
             $options['port'],
             $createStubOptions
@@ -193,28 +233,31 @@ class SpeechApi
     }
 
     /**
-     * Perform synchronous speech-recognition: receive results after all audio
-     * has been sent and processed.
+     * Get the specified group.
      *
      * Sample code:
      * ```
      * try {
-     *     $speechApi = new SpeechApi();
-     *     $config = new RecognitionConfig();
-     *     $audio = new RecognitionAudio();
-     *     $response = $speechApi->syncRecognize($config, $audio);
+     *     $errorGroupServiceApi = new ErrorGroupServiceApi();
+     *     $formattedGroupName = ErrorGroupServiceApi::formatGroupName("[PROJECT]", "[GROUP]");
+     *     $response = $errorGroupServiceApi->getGroup($formattedGroupName);
      * } finally {
-     *     if (isset($speechApi)) {
-     *         $speechApi->close();
+     *     if (isset($errorGroupServiceApi)) {
+     *         $errorGroupServiceApi->close();
      *     }
      * }
      * ```
      *
-     * @param RecognitionConfig $config       [Required] The `config` message provides information to the recognizer
-     *                                        that specifies how to process the request.
-     * @param RecognitionAudio  $audio        [Required] The audio data to be recognized.
-     * @param array             $optionalArgs {
-     *                                        Optional.
+     * @param string $groupName [Required] The group resource name. Written as
+     *                          <code>projects/<var>projectID</var>/groups/<var>group_name</var></code>.
+     *                          Call
+     *                          <a href="/error-reporting/reference/rest/v1beta1/projects.groupStats/list">
+     *                          <code>groupStats.list</code></a> to return a list of groups belonging to
+     *                          this project.
+     *
+     * Example: <code>projects/my-project-123/groups/my-group</code>
+     * @param array $optionalArgs {
+     *                            Optional.
      *
      *     @type Google\GAX\RetrySettings $retrySettings
      *          Retry settings to use for this call. If present, then
@@ -224,24 +267,23 @@ class SpeechApi
      *          is not set.
      * }
      *
-     * @return google\cloud\speech\v1beta1\SyncRecognizeResponse
+     * @return google\devtools\clouderrorreporting\v1beta1\ErrorGroup
      *
      * @throws Google\GAX\ApiException if the remote call fails
      */
-    public function syncRecognize($config, $audio, $optionalArgs = [])
+    public function getGroup($groupName, $optionalArgs = [])
     {
-        $request = new SyncRecognizeRequest();
-        $request->setConfig($config);
-        $request->setAudio($audio);
+        $request = new GetGroupRequest();
+        $request->setGroupName($groupName);
 
-        $mergedSettings = $this->defaultCallSettings['syncRecognize']->merge(
+        $mergedSettings = $this->defaultCallSettings['getGroup']->merge(
             new CallSettings($optionalArgs)
         );
         $callable = ApiCallable::createApiCall(
-            $this->speechStub,
-            'SyncRecognize',
+            $this->errorGroupServiceStub,
+            'GetGroup',
             $mergedSettings,
-            $this->descriptors['syncRecognize']
+            $this->descriptors['getGroup']
         );
 
         return $callable(
@@ -251,30 +293,25 @@ class SpeechApi
     }
 
     /**
-     * Perform asynchronous speech-recognition: receive results via the
-     * google.longrunning.Operations interface. Returns either an
-     * `Operation.error` or an `Operation.response` which contains
-     * an `AsyncRecognizeResponse` message.
+     * Replace the data for the specified group.
+     * Fails if the group does not exist.
      *
      * Sample code:
      * ```
      * try {
-     *     $speechApi = new SpeechApi();
-     *     $config = new RecognitionConfig();
-     *     $audio = new RecognitionAudio();
-     *     $response = $speechApi->asyncRecognize($config, $audio);
+     *     $errorGroupServiceApi = new ErrorGroupServiceApi();
+     *     $group = new ErrorGroup();
+     *     $response = $errorGroupServiceApi->updateGroup($group);
      * } finally {
-     *     if (isset($speechApi)) {
-     *         $speechApi->close();
+     *     if (isset($errorGroupServiceApi)) {
+     *         $errorGroupServiceApi->close();
      *     }
      * }
      * ```
      *
-     * @param RecognitionConfig $config       [Required] The `config` message provides information to the recognizer
-     *                                        that specifies how to process the request.
-     * @param RecognitionAudio  $audio        [Required] The audio data to be recognized.
-     * @param array             $optionalArgs {
-     *                                        Optional.
+     * @param ErrorGroup $group        [Required] The group which replaces the resource on the server.
+     * @param array      $optionalArgs {
+     *                                 Optional.
      *
      *     @type Google\GAX\RetrySettings $retrySettings
      *          Retry settings to use for this call. If present, then
@@ -284,24 +321,23 @@ class SpeechApi
      *          is not set.
      * }
      *
-     * @return google\longrunning\Operation
+     * @return google\devtools\clouderrorreporting\v1beta1\ErrorGroup
      *
      * @throws Google\GAX\ApiException if the remote call fails
      */
-    public function asyncRecognize($config, $audio, $optionalArgs = [])
+    public function updateGroup($group, $optionalArgs = [])
     {
-        $request = new AsyncRecognizeRequest();
-        $request->setConfig($config);
-        $request->setAudio($audio);
+        $request = new UpdateGroupRequest();
+        $request->setGroup($group);
 
-        $mergedSettings = $this->defaultCallSettings['asyncRecognize']->merge(
+        $mergedSettings = $this->defaultCallSettings['updateGroup']->merge(
             new CallSettings($optionalArgs)
         );
         $callable = ApiCallable::createApiCall(
-            $this->speechStub,
-            'AsyncRecognize',
+            $this->errorGroupServiceStub,
+            'UpdateGroup',
             $mergedSettings,
-            $this->descriptors['asyncRecognize']
+            $this->descriptors['updateGroup']
         );
 
         return $callable(
@@ -316,7 +352,7 @@ class SpeechApi
      */
     public function close()
     {
-        $this->speechStub->close();
+        $this->errorGroupServiceStub->close();
     }
 
     private function createCredentialsCallback()
--- generated/php/google-cloud-error-reporting-v1beta1/src/Errorreporting/V1beta1/ReportErrorsServiceApi.php
+++ generated/php/google-cloud-error-reporting-v1beta1/src/Errorreporting/V1beta1/ReportErrorsServiceApi.php
@@ -16,38 +16,37 @@
 /*
  * GENERATED CODE WARNING
  * This file was generated from the file
- * https://github.com/google/googleapis/blob/master/google/cloud/speech/v1beta1/cloud_speech.proto
+ * https://github.com/google/googleapis/blob/master/google/devtools/clouderrorreporting/v1beta1/report_errors_service.proto
  * and updates to that file get reflected here through a refresh process.
  */
 
-namespace Google\Cloud\Speech\V1beta1;
+namespace Google\Cloud\Errorreporting\V1beta1;
 
 use Google\GAX\AgentHeaderDescriptor;
 use Google\GAX\ApiCallable;
 use Google\GAX\CallSettings;
 use Google\GAX\GrpcConstants;
 use Google\GAX\GrpcCredentialsHelper;
-use google\cloud\speech\v1beta1\AsyncRecognizeRequest;
-use google\cloud\speech\v1beta1\RecognitionAudio;
-use google\cloud\speech\v1beta1\RecognitionConfig;
-use google\cloud\speech\v1beta1\SpeechClient;
-use google\cloud\speech\v1beta1\SyncRecognizeRequest;
+use Google\GAX\PathTemplate;
+use google\devtools\clouderrorreporting\v1beta1\ReportErrorEventRequest;
+use google\devtools\clouderrorreporting\v1beta1\ReportErrorsServiceClient;
+use google\devtools\clouderrorreporting\v1beta1\ReportedErrorEvent;
 
 /**
- * Service Description: Service that implements Google Cloud Speech API.
+ * Service Description: An API for reporting error events.
  *
  * This class provides the ability to make remote calls to the backing service through method
  * calls that map to API methods. Sample code to get started:
  *
  * ```
  * try {
- *     $speechApi = new SpeechApi();
- *     $config = new RecognitionConfig();
- *     $audio = new RecognitionAudio();
- *     $response = $speechApi->syncRecognize($config, $audio);
+ *     $reportErrorsServiceApi = new ReportErrorsServiceApi();
+ *     $formattedProjectName = ReportErrorsServiceApi::formatProjectName("[PROJECT]");
+ *     $event = new ReportedErrorEvent();
+ *     $response = $reportErrorsServiceApi->reportErrorEvent($formattedProjectName, $event);
  * } finally {
- *     if (isset($speechApi)) {
- *         $speechApi->close();
+ *     if (isset($reportErrorsServiceApi)) {
+ *         $reportErrorsServiceApi->close();
  *     }
  * }
  * ```
@@ -57,12 +56,12 @@ use google\cloud\speech\v1beta1\SyncRecognizeRequest;
  * a parse method to extract the individual identifiers contained within names that are
  * returned.
  */
-class SpeechApi
+class ReportErrorsServiceApi
 {
     /**
      * The default address of the service.
      */
-    const SERVICE_ADDRESS = 'speech.googleapis.com';
+    const SERVICE_ADDRESS = 'clouderrorreporting.googleapis.com';
 
     /**
      * The default port of the service.
@@ -78,11 +77,42 @@ class SpeechApi
     const _CODEGEN_NAME = 'GAPIC';
     const _CODEGEN_VERSION = '0.0.0';
 
+    private static $projectNameTemplate;
+
     private $grpcCredentialsHelper;
-    private $speechStub;
+    private $reportErrorsServiceStub;
     private $scopes;
     private $defaultCallSettings;
     private $descriptors;
+
+    /**
+     * Formats a string containing the fully-qualified path to represent
+     * a project resource.
+     */
+    public static function formatProjectName($project)
+    {
+        return self::getProjectNameTemplate()->render([
+            'project' => $project,
+        ]);
+    }
+
+    /**
+     * Parses the project from the given fully-qualified path which
+     * represents a project resource.
+     */
+    public static function parseProjectFromProjectName($projectName)
+    {
+        return self::getProjectNameTemplate()->match($projectName)['project'];
+    }
+
+    private static function getProjectNameTemplate()
+    {
+        if (self::$projectNameTemplate == null) {
+            self::$projectNameTemplate = new PathTemplate('projects/{project}');
+        }
+
+        return self::$projectNameTemplate;
+    }
 
     private static function getPageStreamingDescriptors()
     {
@@ -100,14 +130,14 @@ class SpeechApi
      *                       Optional. Options for configuring the service API wrapper.
      *
      *     @type string $serviceAddress The domain name of the API remote host.
-     *                                  Default 'speech.googleapis.com'.
+     *                                  Default 'clouderrorreporting.googleapis.com'.
      *     @type mixed $port The port on which to connect to the remote host. Default 443.
      *     @type Grpc\ChannelCredentials $sslCreds
      *           A `ChannelCredentials` for use with an SSL-enabled channel.
      *           Default: a credentials object returned from
      *           Grpc\ChannelCredentials::createSsl()
      *     @type array $scopes A string array of scopes to use when acquiring credentials.
-     *                         Default the scopes for the Google Cloud Speech API.
+     *                         Default the scopes for the Stackdriver Error Reporting API.
      *     @type array $retryingOverride
      *           An associative array of string => RetryOptions, where the keys
      *           are method names (e.g. 'createFoo'), that overrides default retrying
@@ -153,19 +183,18 @@ class SpeechApi
 
         $defaultDescriptors = ['headerDescriptor' => $headerDescriptor];
         $this->descriptors = [
-            'syncRecognize' => $defaultDescriptors,
-            'asyncRecognize' => $defaultDescriptors,
+            'reportErrorEvent' => $defaultDescriptors,
         ];
         $pageStreamingDescriptors = self::getPageStreamingDescriptors();
         foreach ($pageStreamingDescriptors as $method => $pageStreamingDescriptor) {
             $this->descriptors[$method]['pageStreamingDescriptor'] = $pageStreamingDescriptor;
         }
 
-        $clientConfigJsonString = file_get_contents(__DIR__.'/resources/speech_client_config.json');
+        $clientConfigJsonString = file_get_contents(__DIR__.'/resources/report_errors_service_client_config.json');
         $clientConfig = json_decode($clientConfigJsonString, true);
         $this->defaultCallSettings =
                 CallSettings::load(
-                    'google.cloud.speech.v1beta1.Speech',
+                    'google.devtools.clouderrorreporting.v1beta1.ReportErrorsService',
                     $clientConfig,
                     $options['retryingOverride'],
                     GrpcConstants::getStatusCodeNames(),
@@ -181,11 +210,11 @@ class SpeechApi
         $grpcCredentialsHelperOptions = array_diff_key($options, $defaultOptions);
         $this->grpcCredentialsHelper = new GrpcCredentialsHelper($this->scopes, $grpcCredentialsHelperOptions);
 
-        $createSpeechStubFunction = function ($hostname, $opts) {
-            return new SpeechClient($hostname, $opts);
+        $createReportErrorsServiceStubFunction = function ($hostname, $opts) {
+            return new ReportErrorsServiceClient($hostname, $opts);
         };
-        $this->speechStub = $this->grpcCredentialsHelper->createStub(
-            $createSpeechStubFunction,
+        $this->reportErrorsServiceStub = $this->grpcCredentialsHelper->createStub(
+            $createReportErrorsServiceStubFunction,
             $options['serviceAddress'],
             $options['port'],
             $createStubOptions
@@ -193,28 +222,36 @@ class SpeechApi
     }
 
     /**
-     * Perform synchronous speech-recognition: receive results after all audio
-     * has been sent and processed.
+     * Report an individual error event.
+     *
+     * This endpoint accepts <strong>either</strong> an OAuth token,
+     * <strong>or</strong> an
+     * <a href="https://support.google.com/cloud/answer/6158862">API key</a>
+     * for authentication. To use an API key, append it to the URL as the value of
+     * a `key` parameter. For example:
+     * <pre>POST https://clouderrorreporting.googleapis.com/v1beta1/projects/example-project/events:report?key=123ABC456</pre>
      *
      * Sample code:
      * ```
      * try {
-     *     $speechApi = new SpeechApi();
-     *     $config = new RecognitionConfig();
-     *     $audio = new RecognitionAudio();
-     *     $response = $speechApi->syncRecognize($config, $audio);
+     *     $reportErrorsServiceApi = new ReportErrorsServiceApi();
+     *     $formattedProjectName = ReportErrorsServiceApi::formatProjectName("[PROJECT]");
+     *     $event = new ReportedErrorEvent();
+     *     $response = $reportErrorsServiceApi->reportErrorEvent($formattedProjectName, $event);
      * } finally {
-     *     if (isset($speechApi)) {
-     *         $speechApi->close();
+     *     if (isset($reportErrorsServiceApi)) {
+     *         $reportErrorsServiceApi->close();
      *     }
      * }
      * ```
      *
-     * @param RecognitionConfig $config       [Required] The `config` message provides information to the recognizer
-     *                                        that specifies how to process the request.
-     * @param RecognitionAudio  $audio        [Required] The audio data to be recognized.
-     * @param array             $optionalArgs {
-     *                                        Optional.
+     * @param string             $projectName  [Required] The resource name of the Google Cloud Platform project. Written
+     *                                         as `projects/` plus the
+     *                                         [Google Cloud Platform project ID](https://support.google.com/cloud/answer/6158840).
+     *                                         Example: `projects/my-project-123`.
+     * @param ReportedErrorEvent $event        [Required] The error event to be reported.
+     * @param array              $optionalArgs {
+     *                                         Optional.
      *
      *     @type Google\GAX\RetrySettings $retrySettings
      *          Retry settings to use for this call. If present, then
@@ -224,84 +261,24 @@ class SpeechApi
      *          is not set.
      * }
      *
-     * @return google\cloud\speech\v1beta1\SyncRecognizeResponse
+     * @return google\devtools\clouderrorreporting\v1beta1\ReportErrorEventResponse
      *
      * @throws Google\GAX\ApiException if the remote call fails
      */
-    public function syncRecognize($config, $audio, $optionalArgs = [])
+    public function reportErrorEvent($projectName, $event, $optionalArgs = [])
     {
-        $request = new SyncRecognizeRequest();
-        $request->setConfig($config);
-        $request->setAudio($audio);
+        $request = new ReportErrorEventRequest();
+        $request->setProjectName($projectName);
+        $request->setEvent($event);
 
-        $mergedSettings = $this->defaultCallSettings['syncRecognize']->merge(
+        $mergedSettings = $this->defaultCallSettings['reportErrorEvent']->merge(
             new CallSettings($optionalArgs)
         );
         $callable = ApiCallable::createApiCall(
-            $this->speechStub,
-            'SyncRecognize',
+            $this->reportErrorsServiceStub,
+            'ReportErrorEvent',
             $mergedSettings,
-            $this->descriptors['syncRecognize']
-        );
-
-        return $callable(
-            $request,
-            [],
-            ['call_credentials_callback' => $this->createCredentialsCallback()]);
-    }
-
-    /**
-     * Perform asynchronous speech-recognition: receive results via the
-     * google.longrunning.Operations interface. Returns either an
-     * `Operation.error` or an `Operation.response` which contains
-     * an `AsyncRecognizeResponse` message.
-     *
-     * Sample code:
-     * ```
-     * try {
-     *     $speechApi = new SpeechApi();
-     *     $config = new RecognitionConfig();
-     *     $audio = new RecognitionAudio();
-     *     $response = $speechApi->asyncRecognize($config, $audio);
-     * } finally {
-     *     if (isset($speechApi)) {
-     *         $speechApi->close();
-     *     }
-     * }
-     * ```
-     *
-     * @param RecognitionConfig $config       [Required] The `config` message provides information to the recognizer
-     *                                        that specifies how to process the request.
-     * @param RecognitionAudio  $audio        [Required] The audio data to be recognized.
-     * @param array             $optionalArgs {
-     *                                        Optional.
-     *
-     *     @type Google\GAX\RetrySettings $retrySettings
-     *          Retry settings to use for this call. If present, then
-     *          $timeoutMillis is ignored.
-     *     @type int $timeoutMillis
-     *          Timeout to use for this call. Only used if $retrySettings
-     *          is not set.
-     * }
-     *
-     * @return google\longrunning\Operation
-     *
-     * @throws Google\GAX\ApiException if the remote call fails
-     */
-    public function asyncRecognize($config, $audio, $optionalArgs = [])
-    {
-        $request = new AsyncRecognizeRequest();
-        $request->setConfig($config);
-        $request->setAudio($audio);
-
-        $mergedSettings = $this->defaultCallSettings['asyncRecognize']->merge(
-            new CallSettings($optionalArgs)
-        );
-        $callable = ApiCallable::createApiCall(
-            $this->speechStub,
-            'AsyncRecognize',
-            $mergedSettings,
-            $this->descriptors['asyncRecognize']
+            $this->descriptors['reportErrorEvent']
         );
 
         return $callable(
@@ -316,7 +293,7 @@ class SpeechApi
      */
     public function close()
     {
-        $this->speechStub->close();
+        $this->reportErrorsServiceStub->close();
     }
 
     private function createCredentialsCallback()
