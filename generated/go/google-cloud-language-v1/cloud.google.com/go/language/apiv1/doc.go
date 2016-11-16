053d16d6fd209c5891bdf9ac99bdde6d56b59378
Michael Darakananda <pongad@google.com>
2016-11-17 09:09:56 +1100

    Regenerate Go (#102)
    This brings in a few features.
    
    - The license on top of files changes slightly.
    - Iterators now have InternalFetch method.
    - language/apiv1beta1 -> language/apiv1.

--- generated/go/google-cloud-language-v1/cloud.google.com/go/language/apiv1/doc.go
+++ generated/go/google-cloud-language-v1/cloud.google.com/go/language/apiv1/doc.go
@@ -1,10 +1,10 @@
-// Copyright 2016 Google Inc. All Rights Reserved.
+// Copyright 2016, Google Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,6 +20,6 @@
 // Google Cloud Natural Language API provides natural language understanding
 // technologies to developers. Examples include sentiment analysis, entity
 // recognition, and text annotations.
-package language // import "cloud.google.com/go/language/apiv1beta1"
+package language // import "cloud.google.com/go/language/apiv1"
 
 const gapicNameVersion = "gapic/0.1.0"
