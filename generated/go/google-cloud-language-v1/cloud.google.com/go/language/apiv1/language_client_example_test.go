f2370e399afc043ceb9b35be144a4078ad4f3fb9
Michael Darakananda <pongad@google.com>
2016-11-16 16:03:41 +1100

    Regenerate Go
    This brings in a few features.
    
    - The license on top of files changes slightly.
    - Iterators now have InternalFetch method.
    - language/apiv1beta1 -> language/apiv1.

--- generated/go/google-cloud-language-v1/cloud.google.com/go/language/apiv1/language_client_example_test.go
+++ generated/go/google-cloud-language-v1/cloud.google.com/go/language/apiv1/language_client_example_test.go
@@ -1,10 +1,10 @@
-// Copyright 2016 Google Inc. All Rights Reserved.
+// Copyright 2016, Google Inc. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,9 +17,9 @@
 package language_test
 
 import (
-	"cloud.google.com/go/language/apiv1beta1"
+	"cloud.google.com/go/language/apiv1"
 	"golang.org/x/net/context"
-	languagepb "google.golang.org/genproto/googleapis/cloud/language/v1beta1"
+	languagepb "google.golang.org/genproto/googleapis/cloud/language/v1"
 )
 
 func ExampleNewClient() {
@@ -61,6 +61,24 @@ func ExampleClient_AnalyzeEntities() {
 	// TODO: Fill request struct fields.
 	}
 	resp, err := c.AnalyzeEntities(ctx, req)
+	if err != nil {
+		// TODO: Handle error.
+	}
+	// TODO: Use resp.
+	_ = resp
+}
+
+func ExampleClient_AnalyzeSyntax() {
+	ctx := context.Background()
+	c, err := language.NewClient(ctx)
+	if err != nil {
+		// TODO: Handle error.
+	}
+
+	req := &languagepb.AnalyzeSyntaxRequest{
+	// TODO: Fill request struct fields.
+	}
+	resp, err := c.AnalyzeSyntax(ctx, req)
 	if err != nil {
 		// TODO: Handle error.
 	}
