59038418a92c31c89ba681755d5bb6c58b759362
Jacob Geiger <jgeiger@google.com>
2016-11-30 17:27:36 -0800

    Edit dependency upper bounds

--- generated/python/gapic-google-cloud-logging-v2/setup.py
+++ generated/python/gapic-google-cloud-logging-v2/setup.py
@@ -11,8 +11,7 @@ import sys
 install_requires = [
     'googleapis-common-protos>=1.5.0, <2.0dev',
     'google-gax>=0.15.0, <0.16dev',
-    # TODO (https://github.com/googleapis/packman/issues/119): need upper bound logic
-    'grpc-google-cloud-logging-v2>=0.14.0',
+    'grpc-google-cloud-logging-v2>=0.14.0, <0.15.0dev',
     'oauth2client>=2.0.0, <4.0dev',
 ]
 
