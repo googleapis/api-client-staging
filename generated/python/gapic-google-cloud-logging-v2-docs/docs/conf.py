267d2f35a715b3c1ff1d8da666875e62b25bebf3
Ethan Bao <cbao@google.com>
2016-12-07 10:17:02 -0800

    Add docs/requirements.txt to specified package location for RTD doc generation. Switch back to RTD theme.

--- generated/python/gapic-google-cloud-logging-v2-docs/docs/conf.py
+++ generated/python/gapic-google-cloud-logging-v2-docs/docs/conf.py
@@ -43,7 +43,6 @@ extensions = [
 autoclass_content = 'both'
 autodoc_default_flags = ['members']
 autosummary_generate = True
-numpydoc_show_class_members = False
 
 
 # Add any paths that contain templates here, relative to this directory.
@@ -123,7 +122,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
