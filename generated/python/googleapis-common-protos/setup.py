572db4b695e0f9c027a84a6b36734f47c5ee18a6
Luke Sneeringer <lukesneeringer@google.com>
2017-02-09 14:41:16 -0800

    Support the old import paths; bump version to 1.5.2

--- generated/python/googleapis-common-protos/setup.py
+++ generated/python/googleapis-common-protos/setup.py
@@ -19,7 +19,7 @@ extras_require = {
 
 setuptools.setup(
   name='googleapis-common-protos',
-  version='1.5.1',
+  version='1.5.2',
 
   author='Google Inc',
   author_email='googleapis-packages@google.com',
