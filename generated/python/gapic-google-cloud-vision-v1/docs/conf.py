df9d9351f983f22369c25e146b5138f0c2e9cce4
Luke Sneeringer <lukesneeringer@google.com>
2017-03-03 15:18:10 -0800

    Python Vision GAPIC 0.90.3

--- generated/python/gapic-google-cloud-vision-v1/docs/conf.py
+++ generated/python/gapic-google-cloud-vision-v1/docs/conf.py
@@ -20,7 +20,7 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
-__version__ = '0.15.3'
+__version__ = '0.90.3'
 
 # -- General configuration ------------------------------------------------
 
