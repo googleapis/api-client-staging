1589918408a87c0314325464b38b0bb54b087cea
Luke Sneeringer <lukesneeringer@google.com>
2017-03-06 07:32:54 -0800

    Bump to GAX 0.15.8

--- generated/python/gapic-google-cloud-speech-v1beta1/setup.py
+++ generated/python/gapic-google-cloud-speech-v1beta1/setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 import sys
 
 install_requires = [
-    'google-gax>=0.15.7, <0.16dev',
+    'google-gax>=0.15.8, <0.16dev',
     'oauth2client>=2.0.0, <4.0dev',
     'proto-google-cloud-speech-v1beta1[grpc]>=0.15.3, <0.16dev',
     'googleapis-common-protos[grpc]>=1.5.2, <2.0dev',
