71a3c20f017974335213c6fd55f1f45c6dfe0a21
Luke Sneeringer <lukesneeringer@google.com>
2017-04-27 13:38:39 -0700

    Python GRPC/Proto: datastore v1

--- generated/python/proto-google-cloud-datastore-v1/setup.py
+++ generated/python/proto-google-cloud-datastore-v1/setup.py
@@ -23,7 +23,7 @@ extras_require = {
 
 setuptools.setup(
   name='proto-google-cloud-datastore-v1',
-  version='0.90.3',
+  version='0.90.4',
   author='Google Inc',
   author_email='googleapis-packages@google.com',
   classifiers=[
