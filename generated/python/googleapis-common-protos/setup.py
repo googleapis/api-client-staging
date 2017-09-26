f9ab613d961f5a984fa9614e589240dd7939a816
Luke Sneeringer <lukesneeringer@google.com>
2017-09-26 14:15:19 -0700

    Version number bump.

--- generated/python/googleapis-common-protos/setup.py
+++ generated/python/googleapis-common-protos/setup.py
@@ -19,7 +19,7 @@ extras_require = {
 
 setuptools.setup(
   name='googleapis-common-protos',
-  version='1.5.2',
+  version='1.5.3',
 
   author='Google Inc',
   author_email='googleapis-packages@google.com',
