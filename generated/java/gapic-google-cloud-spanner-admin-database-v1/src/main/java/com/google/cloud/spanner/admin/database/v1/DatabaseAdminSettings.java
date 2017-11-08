2b081e89ad9c38273057e49e92a524bcb14ea53b
Garrett Jones <garrettjonesgoogle@users.noreply.github.com>
2017-11-08 08:22:08 -0800

    Java: Regenerating with surface cleanup

--- generated/java/gapic-google-cloud-spanner-admin-database-v1/src/main/java/com/google/cloud/spanner/admin/database/v1/DatabaseAdminSettings.java
+++ generated/java/gapic-google-cloud-spanner-admin-database-v1/src/main/java/com/google/cloud/spanner/admin/database/v1/DatabaseAdminSettings.java
@@ -253,21 +253,6 @@ public class DatabaseAdminSettings extends ClientSettings<DatabaseAdminSettings>
     return gapicVersion;
   }
 
-  /** Returns a builder for this class with recommended defaults. */
-  @Deprecated
-  public static Builder defaultBuilder() {
-    return Builder.createDefault();
-  }
-
-  /**
-   * Returns a builder for this class with recommended defaults for API methods, and the given
-   * ClientContext used for executor/transport/credentials.
-   */
-  @Deprecated
-  public static Builder defaultBuilder(ClientContext clientContext) {
-    return new Builder(clientContext);
-  }
-
   /** Returns a new builder for this class. */
   public static Builder newBuilder() {
     return Builder.createDefault();
--- generated/java/gapic-google-cloud-spanner-v1/src/test/java/com/google/cloud/spanner/v1/MockSpanner.java
+++ generated/java/gapic-google-cloud-spanner-v1/src/test/java/com/google/cloud/spanner/v1/MockSpanner.java
@@ -15,12 +15,14 @@
  */
 package com.google.cloud.spanner.v1;
 
+import com.google.api.core.BetaApi;
 import com.google.api.gax.grpc.testing.MockGrpcService;
 import com.google.protobuf.GeneratedMessageV3;
 import io.grpc.ServerServiceDefinition;
 import java.util.List;
 
 @javax.annotation.Generated("by GAPIC")
+@BetaApi
 public class MockSpanner implements MockGrpcService {
   private final MockSpannerImpl serviceImpl;
 
